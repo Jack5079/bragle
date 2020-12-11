4cbe9ab60ae850565af2a9118453c14614178448
Jack <29169102+Jack5079@users.noreply.github.com>
2020-12-11 15:36:49 -0500

    h

--- src/handler.client.ts
+++ src/handler.client.ts
@@ -1,9 +1,7 @@
 import { ContextActionService, Players } from '@rbxts/services'
 declare const script: LocalScript & { Parent: Model }
 const input = script.Parent.WaitForChild('input') as RemoteEvent<(name: string, state: Enum.UserInputState, obj: InputObject) => void>
-const handler = (name: string, state: Enum.UserInputState, obj: InputObject) => {
-  input.FireServer(name, state, obj)
-}
+const handler = (name: string, state: Enum.UserInputState, obj: InputObject) => input.FireServer(name, state, obj)
 ContextActionService.BindAction('Kill', handler, false, Enum.KeyCode.E)
 ContextActionService.BindAction('Banish', handler, false, Enum.KeyCode.Q)
 ContextActionService.BindAction('UnbanishAll', handler, true, Enum.KeyCode.F)
